63d19558fdf8677ffb82080475d01281d4d0f00b
cinta1210 <cinta.ramayanti@gmail.com>
2021-07-04 12:43:35 +0700

    fix trip

--- components/Trip.vue
+++ components/Trip.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="xl:mb-20 mb-10 text-center">
-    <img class="rounded-lg block mx-auto" src="~assets/img/pantai.png" />
+    <img class="rounded-lg block mx-auto" src="" />
     <p class="text-grey mt-4 text-xl">OPEN TRIP</p>
     <h3 class="mt-4 font-bold text-2xl">Paket Wisata Nusa Penida - 3D2N</h3>
     <h3 class="mt-4 font-bold text-2xl text-yellow">
--- pages/trip/index.vue
+++ pages/trip/index.vue
@@ -1,53 +1,22 @@
+@@ -0,0 +1,105 @@
 <template>
-  <div class="xl:container mx-auto mt-10 xl:px-16 px-10">
-    <Header active="trip" />
-    <div class="relative w-full mt-10 xl:mb-20 mb-10 overflow-hidden">
-      <h1
-        class="
-          absolute
-          text-white
-          font-bold
-          xl:text-6xl
-          text-3xl
-          xl:left-20
-          xl:top-40
-          left-10
-          top-20
-        "
-      >
-        Trip Nyaman dengan <br />
-        Sandcation.
-      </h1>
-      <img src="~assets/img/trip-hero.png" class="w-full rounded-3xl" />
-    </div>
-    <div class="text-center xl:mb-20 mb-10">
-      <h2 class="font-bold xl:text-5xl text-3xl">Paket Trip</h2>
-      <p class="text-grey xl:text-md text-sm">
-        Ayo cari dan pesan liburanmu sekarang juga
+  <div class="container mx-auto mt-10 my-8">
+    <Header/>
+
+    <div class="grid grid-cols-12 gap-8 -ml- h-3xl sea-img mt-8 leading-loose rounded-2xl">
+      <p class="col-span-7 text-4xl sea-title text-white pl-14">
+        Trip Nyaman dengan <br>
+        Sandcation
       </p>
     </div>
-    <div class="grid xl:grid-cols-3 grid-cols-1 gap-10 xl:mb-20 mb-10">
-      <Trip />
-      <Trip />
-      <Trip />
-      <Trip />
-      <Trip />
-      <Trip />
-      <Trip />
-      <Trip />
-      <Trip />
-    </div>
-    <Footer />
-  </div>
-</template>
 
     <div class="paket-text">
-      <p class="mt-12 font-bold text-black text-3xl">Paket trip</p>
+      <p class="mt-12 font-bold text-black text-3xl">Paket Trip</p>
     </div>
 
     <div class="flex justify-center mt-12">
       <div class="mx-12 ">
-        <img src="~/assets/img/nusa-peninda.png" alt="">
+        <img src="~/assets/img/Pantai.png" alt="">
         <div class="text-center">
           <p class="text-gray-400">open trip</p>
           <p class="text-2xl font-poppins font-bold">Paket Wisata Nusa <br>
