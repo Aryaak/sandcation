0348ec1b5bd6df750e423bc7be86568311aa9a35
aryaaak <aryarizky2303@gmail.com>
2021-07-05 02:37:57 +0700

    make it better

---
+++ components/Trip.vue
@@ -0,0 +1,20 @@
+<template>
+  <nuxt-link to="/trip/1">
+    <div class="xl:mb-20 mb-10 text-center">
+      <img class="rounded-lg block mx-auto" src="~assets/img/pantai.png" />
+      <p class="text-grey mt-4 text-xl">OPEN TRIP</p>
+      <h3 class="mt-4 font-bold text-2xl">Paket Wisata Nusa Penida - 3D2N</h3>
+      <h3 class="mt-4 font-bold text-2xl text-yellow">
+        <span class="text-grey text-lg font-normal">Mulai dari</span>
+        IDR1.500.000
+      </h3>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
